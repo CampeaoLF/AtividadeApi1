6f8a557a4e24297a8fc175494c740dd6f4e81da6
Felipe Mendes <lfgamestm1@gmail.com>
2025-11-12 21:38:45 -0300

    Atividade Apii

---
+++ Api2/Assets/Scripts/API_Exemplo_Aula/Models/Jogador.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+
+[System.Serializable]
+public class Jogador
+{
+    public string id;
+    public int Item;
+    public int Vida;
+    public float PosicaoX;
+    public float PosicaoY;
+    public float PosicaoZ;
+}
